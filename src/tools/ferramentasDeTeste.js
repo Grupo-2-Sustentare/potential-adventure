62f54eb4b5bc3f21599ed1fc40232a6e58d3ac75
PereiraGus <gustavo.oito@hotmail.com>
2024-09-28 17:30:40 -0300

    55 - Melhorando os métodos de teste.

--- src/tools/ferramentasDeTeste.js
+++ src/tools/ferramentasDeTeste.js
@@ -1,10 +1,14 @@
+function gerarNumeroAleatorio(minimo, maximo){
+  return Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+}
+
 function gerarNumerosAleatorios(quantidade, minimo, maximo) {
   const numeros = [];
   for (let i = 0; i < quantidade; i++) {
-    const numeroAleatorio = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
+    const numeroAleatorio = gerarNumeroAleatorio(minimo, maximo)
     numeros.push(numeroAleatorio);
   }
   return numeros;
 }
 
-export {gerarNumerosAleatorios}
+export {gerarNumeroAleatorio, gerarNumerosAleatorios}
