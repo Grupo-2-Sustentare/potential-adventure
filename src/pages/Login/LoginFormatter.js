eca107586029da801e1b268962d46af0699cf542
PereiraGus <gustavo.oito@hotmail.com>
2024-10-27 13:12:20 -0300

    89 - Adição de post na API + integrando login.

--- src/pages/Login/LoginFormatter.js
+++ src/pages/Login/LoginFormatter.js
@@ -1,39 +1,21 @@
-import axios from "axios";
-import {errorToast} from "../../components/Toast/Toast";
-import {API_URL} from "../../tools/api";
+import {post} from "../../tools/api";
+import {alertToast} from "../../components/Toast/Toast";
 
-const DEBUG_MODE = true
+const DEBUG_MODE = false
 
-export default function login(body){
+export default async function login(body){
     if (DEBUG_MODE) {
         return {"nome": "teste", "senha": ""}
     }
 
-    return axios.get(API_URL + "/usuarios").then((res) => {
-        if(res.data.length === 0){ // TODO - Trocar por "res.status === 204 "
+    let res = await post("usuarios/login", null, body)
+    switch (res.status){
+        case 200:
+            return res.data
+        case 401:
+            alertToast("Usuário ou senha inválidos.")
             return null
-        }
-        return res.data
-    }).catch((err) => {
-        switch (err.response.status) {
-            default:
-                errorToast(`Erro desconhecido, código ${err.response.status}\nContate o suporte.`)
-                return null
-        }
-    });
-
-    return axios.post(API_URL + "/usuarios", body).then((res) => {
-        if(res.data.length === 0){
+        default:
             return null
-        }
-        return res.status
-    }).catch((err) => {
-        switch (err.response.status) {
-            case 400:
-                errorToast(`Não foi possível se conectar com o sistema de login.\nContate o suporte.`)
-                break
-            default:
-                errorToast(`Erro desconhecido, código ${err.response.status}\nContate o suporte.`)
-        }
-    });
+    }
 }
--- src/tools/api.js
+++ src/tools/api.js
@@ -26,4 +26,31 @@ async function get(entidade, params){
     });
 }
 
-export {API_URL, get};
+async function post(entidade, params, body){
+    params = params || {}
+    body = body || {}
+
+    return axios.post(API_URL + entidade, body).then((res) => {
+        if(res.data.length === 0){
+            alert("teste")
+        }
+        return res
+    }).catch((err) => {
+        if (err.response === undefined){
+            errorToast(`Erro desconhecido. \nContate o suporte.`)
+            return
+        }
+        switch (err.response.status) {
+            case 400:
+                errorToast(`Não foi possível se conectar com o sistema de login.\nContate o suporte.`)
+                break
+            case 401:
+                break
+            default:
+                errorToast(`Erro desconhecido, código ${err.response.status}\nContate o suporte.`)
+        }
+        return err;
+    });
+}
+
+export {get, post};
