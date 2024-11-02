1f24682d83ef667a51185990c21c1bf573208847
PereiraGus <gustavo.oito@hotmail.com>
2024-11-02 12:50:13 -0300

    92 - Reorganizando css gráfico + informação de sem dados.

--- src/components/BarChart/BarChart.jsx
+++ src/components/BarChart/BarChart.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, defaults } from 'chart.js';
+import styles from "./barChart.module.css"
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 const CORES = ["#22333B","#E36419","#634B3D"];
 
 const BarChart = (
-        { labels, datasets, title, width, height, backgroundColor, xLabel=undefined, yLabel="Valor",
+        { labels, datasets, title, width, height, xLabel=undefined, yLabel="Valor",
             floatScale = false }
     ) => {
-    const data = {
-        labels: labels,
-        datasets: datasets,
-    };
+    const data = {labels: labels, datasets: datasets};
 
     // Definindo dinâmicamente as cores das categorias dos gráficos via nossa paleta.
     let c = 0
@@ -23,17 +21,13 @@ const BarChart = (
         }
     }
 
+    defaults.color = "#fff";
     const options = {
         responsive: true,
         maintainAspectRatio: false, // Permite que o gráfico ocupe 100% da div
         plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: title || 'Custom Bar Chart',
-            },
+            legend: {position: 'top',},
+            title: {display: true, text: title},
         },
         scales: {
             x: {
@@ -52,32 +46,17 @@ const BarChart = (
             },
         },
     };
+    let conteudo = <div className={styles.noData}>
+        <p>Gráfico de {title}</p>
+        <p>Sem dados para o período e filtros selecionados</p>
+    </div>
+    if (datasets !== null){
+        conteudo = <Bar data={data} options={options} className={styles.barChart} />
+    }
 
     return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: width || '100%',
-            height: height || '100%',
-            backgroundColor: backgroundColor || 'white',
-            padding: '0px',
-            borderRadius: '8px',
-            margin: '0px'
-        }}>
-            <div style={{
-                width: '100%',
-                height: '100%',
-                margin: '0px',
-                padding: '4px 8px',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}>
-                <div style={{ width: '100%', height: '100%' }}>
-                    <Bar data={data} options={options} style={{ width: '100%', height: '100%' }} />
-                </div>
-            </div>
+        <div className={styles.chartContainer} style={{height: height, width: width}}>
+            {conteudo}
         </div>
     );
 };
