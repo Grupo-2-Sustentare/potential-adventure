952fc2580860af73c089fef30d04bbe343520f53
Alexandra Harumi Yara <alexandra.yara@sptech.school>
2024-12-02 19:02:58 -0300

    Lógica de local e nuvem

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,18 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+// True quando local
+const DEBUG = true
+
+// False quando em nuvem
+// const DEBUG = false 
+
+// DEBUG ? 'http://localhost:8080' : '35.200.242.24:8080/
 module.exports = function(app) {
     app.use(
         '/java-api',
         createProxyMiddleware({
             //35.200.242.24:8080
-            target: '35.200.242.24:8080/',
+            target: DEBUG ? 'http://localhost:8080' : '35.200.242.24:8080',
             changeOrigin: true,
         })
     )
