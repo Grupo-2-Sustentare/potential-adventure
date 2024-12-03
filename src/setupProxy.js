5ec7f1c0dbcf4f4d955a4072dbe90a0a65f1eb0c
PereiraGus <gustavo.oito@hotmail.com>
2024-12-03 17:41:19 -0300

    Ajuste no proxy.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
     app.use(
         '/java-api',
         createProxyMiddleware({
-            target: DEBUG ? 'http://localhost:8080' : 'http://10.0.4.97:80/',
+            target: DEBUG ? 'http://localhost:80' : 'http://10.0.4.97:80/',
             changeOrigin: true,
         })
     )
