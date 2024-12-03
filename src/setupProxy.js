a4e288032eb78c330b5447c5bcd84acc8710b414
PereiraGus <gustavo.oito@hotmail.com>
2024-12-03 17:23:54 -0300

    Correção no proxy.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // True quando local
-const DEBUG = false
-
 // False quando em nuvem
-// const DEBUG = false 
+const DEBUG = false
 
 // DEBUG ? 'http://localhost:8080' : '35.200.242.24:8080/
 module.exports = function(app) {
     app.use(
         '/java-api',
         createProxyMiddleware({
-            target: DEBUG ? 'http://localhost:80' : 'http://10.0.4.97:80/',
+            target: DEBUG ? 'http://localhost:8080' : 'http://10.0.4.97:80/',
             changeOrigin: true,
         })
     )
