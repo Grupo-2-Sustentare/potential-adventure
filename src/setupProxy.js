3a9b0f6257974e0e74426b85c4a699ac724a8cd2
PereiraGus <gustavo.oito@hotmail.com>
2024-12-03 18:34:02 -0300

    Correção das portas do proxy.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,12 +4,11 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 // False quando em nuvem
 const DEBUG = true
 
-// DEBUG ? 'http://localhost:8080' : '35.200.242.24:8080/
 module.exports = function(app) {
     app.use(
         '/java-api',
         createProxyMiddleware({
-            target: DEBUG ? 'http://localhost:80' : 'http://10.0.4.97:80/',
+            target: DEBUG ? 'http://localhost:9000' : 'http://10.0.4.97:9000/',
             changeOrigin: true,
         })
     )
