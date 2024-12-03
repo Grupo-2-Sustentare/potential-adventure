1c0961f5fcb5bb9d01448b8ed4eb94647f3dcc48
RaphaelKleinF3 <ralmeida@f3capital.com.br>
2024-12-03 18:18:17 -0300

    eba proxy legal demais

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,14 +2,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // True quando local
 // False quando em nuvem
-const DEBUG = true
+const DEBUG = false
 
 // DEBUG ? 'http://localhost:8080' : '35.200.242.24:8080/
 module.exports = function(app) {
     app.use(
         '/java-api',
         createProxyMiddleware({
-            target: DEBUG ? 'http://localhost:80' : 'http://10.0.4.97:80/',
+            target: DEBUG ? 'http://localhost:80' : 'http://10.0.3.228:9000/',
             changeOrigin: true,
         })
     )
