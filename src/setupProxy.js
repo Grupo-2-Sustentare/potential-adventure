450ca7a35b9eba3c3fb6250d27b741d8c52dcd8d
Julia-Justino <stephany.justino@sptech.school>
2024-12-03 23:39:59 -0300

    ajuste no proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,33 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// True quando local
-// False quando em nuvem
-const DEBUG = false
+// Defina como true para ambiente local, ou false para ambiente em nuvem
+const DEBUG = false;
 
-module.exports = function(app) {
+// Função que configura o proxy
+module.exports = function (app) {
+    // Verifica se a variável DEBUG está definida corretamente
+    const targetUrl = DEBUG ? 'http://localhost:9000' : 'http://10.0.3.149:9000/';
+
+    console.log(`Configuração de proxy ativa. Target: ${targetUrl}`);
+
     app.use(
         '/java-api',
         createProxyMiddleware({
-            target: DEBUG ? 'http://localhost:9000' : 'http://10.0.3.149:9000/',
+            target: targetUrl,
             changeOrigin: true,
+            onError: (err, req, res) => {
+                console.error('Erro no Proxy:', err.message);
+                res.writeHead(500, {
+                    'Content-Type': 'text/plain',
+                });
+                res.end('Algo deu errado no proxy.');
+            },
+            onProxyReq: (proxyReq, req, res) => {
+                console.log(`Proxying request - Path: ${req.path}, Method: ${req.method}`);
+            },
+            onProxyRes: (proxyRes, req, res) => {
+                console.log(`Response recebido do proxy - Status: ${proxyRes.statusCode}`);
+            },
         })
-    )
-}
+    );
+};
